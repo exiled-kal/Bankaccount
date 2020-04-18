40cbaf5dd6b8421698efe4671b934946dfc2a619
kaldne <tseringkalden1964@gmail.com>
2020-04-17 23:15:01 -0700

    first commit

---
+++ src/main/java/com/tseringkalden/bankaccount/BankAccuntApplication.java
@@ -0,0 +1,13 @@
+package com.tseringkalden.bankaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAccuntApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tseringkalden/bankaccount/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.tseringkalden.bankaccount;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BankAccuntApplication.class);
+	}
+
+}
